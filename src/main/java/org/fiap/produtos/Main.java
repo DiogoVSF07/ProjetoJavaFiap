98d1364ced82f8e9cecd97eda9b23d447ec85279
Diogo Victor Sampaio De Farias Leal <diogo.farias@cwi.com.br>
2025-11-16 01:39:18 -0300

    criação das class categorias e produtos

--- src/main/java/org/fiap/produtos/Main.java
+++ src/main/java/org/fiap/produtos/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.fiap.produtos;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/org/fiap/produtos/model/Categoria.java
@@ -0,0 +1,55 @@
+package org.fiap.produtos.model;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome.toUpperCase();
+    }
+
+}
